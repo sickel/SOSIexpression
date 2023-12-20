b22644032272665b12b55e83f4366b598fa880fd
Morten Sickel <morten@sickel.net>
2023-12-20 23:58:06 +0100

    More tests

--- test/test_SOSIexpressions.py
+++ test/test_SOSIexpressions.py
@@ -8,7 +8,7 @@ class testSOSI(unittest.TestCase):
     
     def test_rotate_from_vector00(self):
         field="0 0"
-        self.assertEqual(functions.rotate_from_vector.function(field,None,None),90,f"trying {field}")
+        self.assertEqual(functions.rotate_from_vector.function(field,None,None),0,f"trying {field}")
     
     
     def test_rotate_from_vector01(self):
@@ -25,15 +25,18 @@ class testSOSI(unittest.TestCase):
 
     def test_rotate_from_vector04(self):
         field="1 -1"
-        self.assertEqual(functions.rotate_from_vector.function(field,None,None),135,f"trying {field}")
+        self.assertEqual(functions.rotate_from_vector.function(field,None,None),-45,f"trying {field}")
 
     def test_rotate_from_vector05(self):
         field="0 -1"
-        self.assertEqual(functions.rotate_from_vector.function(field,None,None),180,f"trying {field}")
+        self.assertEqual(functions.rotate_from_vector.function(field,None,None),-90,f"trying {field}")
                 
     def test_rotate_from_vector06(self):
         field="-1 -1"
-        self.assertEqual(functions.rotate_from_vector.function(field,None,None),225,f"trying {field}")
+        self.assertEqual(functions.rotate_from_vector.function(field,None,None),-135,f"trying {field}")
 
-    
+    def test_rotate_from_vector07(self):
+        field="-1 0"
+        self.assertEqual(functions.rotate_from_vector.function(field,None,None),180,f"trying {field}")
+
 

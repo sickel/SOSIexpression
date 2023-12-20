dfc3a8bba507d4843fd33a9013ea467ab3b6b3f7
Morten Sickel <morten@sickel.net>
2023-12-20 23:58:38 +0100

    Using atan2, but this gives wrong direction and starts from east rather than north

--- functions.py
+++ functions.py
@@ -39,29 +39,24 @@ import math
 @qgsfunction(args='auto', group='SOSI',usesgeometry = False)
 def rotate_from_vector(vectorfield,feature,parent):
 
-   """
-   From a field that can be None or a vector given as
-   two space separated floating point numbers, return a rotation
-   angle
+    """
+    From a field that can be None or a vector given as
+    two space separated floating point numbers, return a rotation
+    angle
 
-   :param vectorfield: Name of the field containing the vector data
-   :return: Rotation in degrees clockwise
-   """
+    :param vectorfield: Name of the field containing the vector data
+    :return: Rotation in degrees clockwise
+    """
 
-   if vectorfield is None:
-       return(0)
-   (x,y) = vectorfield.split(" ")
-   x = float(x)
-   if x == 0:
-        rotate = 90
-        if y == 0:
-            rotate = 0
-   y = float(y)
-   tng = y/x
-   rotate = math.atan(tng) / math.pi *180
-   if x < 0:
-       rotate = rotate + 180
-   return(rotate)
+    if vectorfield is None:
+        return(0)
+    (x,y) = vectorfield.split(" ")
+    x = float(x)
+    y = float(y)
+    rotate = math.atan2(y,x) / math.pi *180
+    # This rotation is based on 1,0 is 0 degrees, then goes CCW, 
+    # We want 0 1 to be 0 degrees and then go CW
+    return rotate
 
 
 
@@ -70,7 +65,7 @@ def registerFunctions(isRegister=True):
     t_register = (QgsExpression.registerFunction, lambda f: f)
     u_register = (QgsExpression.unregisterFunction, lambda f: f.name())
     (funcReg, funcArg) = t_register if isRegister else u_register
-    g = globals()
+    # g = globals()
     l_func = (g[v] for v in g if hasattr(g[v], 'usesGeometry'))
     for f in l_func:
         funcReg(funcArg(f))
